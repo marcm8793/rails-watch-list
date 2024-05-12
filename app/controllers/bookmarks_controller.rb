cdc81a8f13f1670f93ad08657a8d06c09aae3610
Marc Mansour <marcm9387@gmail.com>
2024-05-12 17:56:57 +0200

    adding modals and features

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: [ :new, :create ]
+  before_action :set_bookmark, only: :destroy
 
   def new
     @bookmark = Bookmark.new
@@ -9,13 +10,16 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list)
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
@@ -27,4 +31,9 @@ class BookmarksController < ApplicationController
   def set_list
     @list = List.find(params[:list_id])
   end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    list
     @bookmark = Bookmark.new
-    @bookmarks = @list.bookmarks
   end
 
   def new
@@ -22,13 +22,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
   end
 
-  def list
+  def set_list
     @list = List.find(params[:id])
   end
 end
