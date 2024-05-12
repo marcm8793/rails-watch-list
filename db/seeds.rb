7c537c88f4346f1d2547ef92af330160f56c1785
Marc Mansour <marcm9387@gmail.com>
2024-05-12 20:56:54 +0200

    add features

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,34 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-puts 'Seed movie in db'
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-puts 'movies added'
+# puts 'Seed movie in db'
+# Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+# Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+# Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+# Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+# puts 'movies added'
+
+
+
+response = URI.open("https://tmdb.lewagon.com/movie/top_rated?page=20")
+json = JSON.parse(response.read)
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts 'Creating movie in the DB...'
+
+json['results'].each do |entry|
+  movie = Movie.create!(
+    title: entry['title'],
+    overview: entry['overview'],
+    rating: entry['vote_average'],
+    poster_url: "https://image.tmdb.org/t/p/original#{entry['backdrop_path']}"
+  )
+
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
